8c08ec369ab912b4e5b9c05d8dc9f85e7efa71d6
Yaroslava Hryzadubova <yaroslava.gd@gmail.com>
2025-04-22 15:51:10 +0200

    feat: implement fs-create

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,23 @@
+import { writeFile } from 'node:fs/promises';
+import { fileURLToPath } from 'node:url';
+import { dirname, join } from 'node:path';
+
+const __fileName = fileURLToPath(import.meta.url);
+const __dirName = dirname(__fileName);
+
+const filePath = join(__dirName, 'files', 'fresh.txt');
+const fileContent = 'I am fresh and young';
+
 const create = async () => {
-    // Write your code here 
+    try {
+        await writeFile(
+            filePath,
+            fileContent,
+            { flag: 'wx' }
+        );
+    } catch {
+        throw Error('FS operation failed');
+    }
 };
 
 await create();
