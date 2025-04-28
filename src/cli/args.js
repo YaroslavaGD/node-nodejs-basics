94274b8830b8e6f91eb2a90a996396915ae1fcdd
Yaroslava Hryzadubova <yaroslava.gd@gmail.com>
2025-04-28 12:57:15 +0200

    feat: implement cli-args

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,22 @@
 const parseArgs = () => {
-    // Write your code here 
+    const args = process.argv.slice(2);
+    const parsedArgs = args.reduce((acc, current, index) => {
+        if (current.startsWith('--')) {
+            const key = current.slice(2);
+            const value = args[index + 1];
+
+            if (!value || value.startsWith('--')) {
+                console.error(`Missing value for ${current}`);
+                process.exit(1);
+            }
+            acc[key] = value;
+        }
+        return acc;
+    }, {});
+
+    Object.entries(parsedArgs).map(([key, value]) => {
+        console.log(`${key} is ${value}`);
+    });
 };
 
 parseArgs();
