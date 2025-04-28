369b027746a6b8e1a8cdf593f55c631d3fb93bf2
Yaroslava Hryzadubova <yaroslava.gd@gmail.com>
2025-04-28 22:05:27 +0200

    feat: implement streams-write

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,29 @@
+import { fileURLToPath } from "node:url";
+import { dirname, join } from "node:path";
+import { createWriteStream } from "node:fs";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const fileToWrite = join(__dirname, 'files', 'fileToWrite.txt');
+
 const write = async () => {
-    // Write your code here 
+    try {
+        const writeStream = createWriteStream(fileToWrite, { encoding: 'utf-8', flags: 'a' });
+        
+        process.stdin.pipe(writeStream);
+
+        writeStream.on('error', (err) => {
+            console.error('Error. Failed to write file:', err.message);
+        });
+
+        writeStream.on('finish', () => {
+            console.log('Writing completed.');
+        });
+
+    } catch (err) {
+        console.log('Error: ', err.message);
+    }
 };
 
 await write();
