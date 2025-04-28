fc634ed7b2f6dddf0171d80610a9dcbd340c4bc7
Yaroslava Hryzadubova <yaroslava.gd@gmail.com>
2025-04-28 23:03:40 +0200

    feat: implement hash-calcHash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,29 @@
+import { fileURLToPath } from "node:url";
+import { dirname, join } from "node:path";
+import { createHash } from 'node:crypto';
+import { createReadStream } from "node:fs";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const fileForHash = join(__dirname, 'files', 'fileToCalculateHashFor.txt');
+
 const calculateHash = async () => {
-    // Write your code here 
+    try {
+        const readStream = createReadStream(fileForHash);
+        const hash = createHash('sha256');
+    
+        readStream.on('data', (data) => {
+            hash.update(data);
+        });
+
+        readStream.on('end', () => {
+            console.log(hash.digest('hex'));
+        });
+    } catch (err) {
+        console.error(err);
+    }
+
 };
 
 await calculateHash();
