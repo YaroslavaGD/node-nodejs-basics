73cf29e2e90136fba130e51b5c68a32e04788e70
Yaroslava Hryzadubova <yaroslava.gd@gmail.com>
2025-04-28 22:37:52 +0200

    feat: implement stream-transform

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,17 @@
+import { Transform } from "node:stream";
+import { pipeline } from "node:stream/promises";
+
 const transform = async () => {
-    // Write your code here 
+    const transformStream = new Transform({
+        transform(chunk, encoding, callback) {
+            const input = chunk.toString();
+            const reversed = input.split('').reverse().join('') + "\n";
+            callback(null, reversed);
+        }
+    });
+    console.log('Enter any text you want. To end input, press Ctrl+Z.');
+
+    await pipeline(process.stdin, transformStream, process.stdout);
 };
 
 await transform();
